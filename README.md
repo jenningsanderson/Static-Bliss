9b73022add137eca52b2c26dcd778d26c1b051ad
Jennings Anderson <jennings.anderson@colorado.edu>
2014-06-18 01:15:09 -0600

    it's updated again

--- README.md
+++ README.md
@@ -18,10 +18,12 @@ Modify your ````_config.yml```` file to include the following values:
 
 Additionally, create another file called ````_secret_config.yml```` with the following contents:
 ````
-google_username : "you@google.com"
+google_username : "you@gmail.com"
 google_password : "qwerty1"
 
 s3_id : "S3 ID"
 s3_secret : "S3 Secret"
 production_bucket: "yourbucketnamehere"
 ````
+
+Be sure to add the following line to your _.gitignore_ file: ````_secret*.*````
--- lib/Static-Bliss.rb
+++ lib/Static-Bliss.rb
@@ -1,18 +1,73 @@
+require 'yaml'
+
 class StaticBliss
-	require_relative 'aws/bucket_manager'
-	require_relative 'google_drive/object_types'
-	require_relative 'google_drive/parse_to_yaml'
 
+	def initialize(command, args)
+		puts "Command: #{command}"
+		puts "args: #{args}"
 
-	# This should eventually accept all the credentials to make EVERYTHING work... THAT
-	# would be really, really cool, or perhaps it just calls from the config file? maybe
-	# it can parse the config.yml --- err, not
-	def initialize
-		puts "Initialized Static Bliss"
+		puts "Reading the _config.yml file"
+		read_config
+
+		unless @config.nil?
+			build_update_functions
+		end
+
+		begin
+			instance_eval "#{command} (#{args})"
+		rescue
+			"oops, failed"
+		end
+
+		puts self.methods
+
 	end
-	
-	def self.hi
-		puts "Hello world!"
+
+	def read_config
+		if File.exists?('_config.yml')
+			puts "Loading site_config from _config.yml"
+			@site_config = YAML::load(File.open('_config.yml'))
+		else
+			puts "Entering test environment, _config.yml does not exist"
+			@site_config = YAML::load(File.open('spec/_config.yml'))
+		end
+
+		if File.exists?(@site_config['credentials'])
+			puts "Loading credentials from site_config location"
+			@credentials = YAML::load(File.open(@site_config['credentials']))
+		else
+			puts "You do not have a proper credentials file defined in _config.yml"
+		end
 	end
+
+	def publish( *args )
+		puts "Publishing the site to: #{@credentials['production_bucket']}"
+		
+		#Require the bucket manager and make the connection, then publish
+		require_relative 'aws/bucket_manager'
+		@manager = BucketManager.new(@credentials['s3_id'], @credentials['s3_secret'])
+    	@manager.connect_to_bucket(@credentials['production_bucket'])
+
+    	`jekyll build --destination _to_upload`
+  	
+  		@manager.write_directory('_to_upload')
+	end
+
+	def update(args)
+		puts "Reached update with args:"
+		puts "Args: #{args}"
+
+		require_relative 'google_drive/object_types'
+		require_relative 'google_drive/parse_to_yaml'
+
+		#Now it will call the appropriate update function, based on the arguments passed.
+
+		unless args.include? 'all'
+			#update all the pages here, implying, call the update function for all objects
+		else
+			#Call the update function for what's available
+		end
+
+	end
+
 end
-
